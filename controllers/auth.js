a593d387518ea331e0d803365260d7fddf95084e
vzdrizhni <vzdrizhni@gmail.com>
2021-01-06 14:13:09 +0500

    Style error messages

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,19 +2,32 @@ const User = require('../models/user');
 const bcrypt = require('bcryptjs')
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
     isAuthenticated: false,
-    errorMessage: req.flash('error')
+    errorMessage: message
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render('auth/signup', {
     path: '/signup',
     pageTitle: 'Signup',
-    isAuthenticated: false
+    isAuthenticated: false,
+    errorMessage: message
   });
 };
 
@@ -40,6 +53,7 @@ exports.postLogin = (req, res, next) => {
                 res.redirect('/');
               });
           }
+          req.flash('error', 'Invalid email or password')
           return res.redirect('/login')
         })
         .catch(err => console.log(err))
@@ -56,6 +70,7 @@ exports.postSignup = (req, res, next) => {
     .findOne({email: email})
     .then(userDoc => {
       if (userDoc) {
+        req.flash('error', 'Email already exists')
         return res.redirect('/signup')
       }
       return bcrypt
